50eba1ce318201995049f22e2bf0de4d07c7885b
Stuart Sewell <stuart.sewell@nurole.com>
2021-06-14 21:31:10 +0100

    Set up image upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<UploadImage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UploadImage from "./components/UploadImage.vue";
+import "./assets/App.css";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		UploadImage,
+	},
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
